eec6e4649ad4ff5acf86b6a7ad3b050b305fad5f
jeffeddieqin <1925729042@qq.com>
2025-12-12 11:19:29 +0800

    1212am

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import ElementPlus from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
 
 import App from './App.vue'
 import router from './router'
@@ -16,8 +19,8 @@ pinia.use(piniaPluginPersistedstate)
 app.use(pinia)
 app.use(router)
 
-app.config.globalProperties.$ELEMENT = {
-  locale: zhCn,
-}
+// Element Plus 全局中文配置 + dayjs 本地化（用于日期选择器的月份/星期名称）
+app.use(ElementPlus, { locale: zhCn })
+dayjs.locale('zh-cn')
 
 app.mount('#app')
